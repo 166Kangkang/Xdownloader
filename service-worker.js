50a3f5e438fb02ec47f7e94ffb94848dbf3f0184
刘凯 <75723854+166Kangkang@users.noreply.github.com>
2025-05-05 19:07:36 +0800

    sw里面的fetch里面有一个路径错误

--- service-worker.js
+++ service-worker.js
@@ -36,7 +36,7 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.mode === "navigate") {
-    event.respondWith(caches.match("/"));
+    event.respondWith(caches.match("/Xdownloader"));
     return;
   }
 
